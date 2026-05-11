adc865785800166a7b882416176481a6a667696d
Rikyann270 <rikyann270@gmail.com>
2026-05-11 20:38:31 +0300

    fixing

--- server.py
+++ server.py
@@ -525,4 +525,5 @@ async def mark_endpoint(data: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
